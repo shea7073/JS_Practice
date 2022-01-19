0776692f11c610b675cb6cc68b775224384b5d4e
shea7073 <shea7073@gmail.com>
2022-01-19 11:33:30 -0500

    work push

--- fundamentals1/script.js
+++ fundamentals1/script.js
@@ -1,3 +1,13 @@
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+const markHeight = 1.69;
+const markWeight = 78;
+const johnHeight = 1.95;
+const johnWeight = 92;
 
+const markBMI = markWeight / (markHeight ** 2);
+const johnBMI = johnWeight / (johnHeight ** 2);
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI);
+console.log(johnBMI);
+console.log(markHigherBMI);
