631a2a9be312d7611a0be2615dd5631e746c56a0
shea7073 <shea7073@gmail.com>
2022-01-20 11:16:29 -0500

    work push

--- fundamentals1/script.js
+++ fundamentals1/script.js
@@ -8,6 +8,14 @@ const johnBMI = johnWeight / (johnHeight ** 2);
 
 const markHigherBMI = markBMI > johnBMI;
 
+/*
 console.log(markBMI);
 console.log(johnBMI);
 console.log(markHigherBMI);
+*/
+
+if (markBMI > johnBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI}`)
+} else {
+    console.log(`John's BMI (${johnBMI} is higher than Mark's (${markBMI})`)
+}
