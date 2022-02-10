7908437a424756bbd341775b1173638bcd72828f
shea7073 <shea7073@gmail.com>
2022-02-10 09:01:34 -0500

    array stuff, from work

--- fundamentals2/script.js
+++ fundamentals2/script.js
@@ -1,4 +1,53 @@
 'use strict';
 
+// CHALLENGE 1
+// const koala1 = 23;
+// const koala2 = 34;
+// const koala3 = 27;
+//
+// const dolphin1 = 85;
+// const doplpin2 = 54;
+// const dolphin3 = 41;
+//
+// const calcAvg = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+//
+// const koalaAvg = calcAvg(koala3, koala1, koala2);
+// const dolphinAvg = calcAvg(doplpin2, dolphin1, dolphin3);
+//
+// function checkWinner(avgKoala, avgDolphin) {
+//     if (avgKoala >= (avgDolphin * 2)){
+//         console.log(`Koalas win ${avgKoala} to ${avgDolphin}`);
+//     } else if (avgDolphin >= (avgKoala * 2)) {
+//         console.log(`Dolphions win ${avgDolphin} to ${avgKoala}`);
+//     } else {
+//         console.log('There is no winner!');
+//     }
+// }
+//
+// checkWinner(koalaAvg, dolphinAvg);
+
+// CHALLENGE 2
+const ternary_tip = function(bill) {
+    return (bill >= 50 && bill <= 300) ? bill * .2 : bill * .15;
+}
+console.log(ternary_tip(500));
 
 
+function calcTip(bill) {
+    let tip;
+    if (bill <= 50 && bill <= 300){
+        tip = bill * .15
+    } else {
+        tip = bill * .2
+    }
+    return tip
+}
+
+const bills = [125, 555, 44]
+console.log(bills)
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])]
+console.log(tips)
+
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
+
+console.log(totals)
