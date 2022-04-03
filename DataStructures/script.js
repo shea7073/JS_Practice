9a51f06e8f92fad0426cf1a08b7539a556cbe8eb
shea7073 <shea7073@gmail.com>
2022-04-03 15:30:42 -0400

    mac

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -4,9 +4,10 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
 const openingHours = {
-  thu: {
+  //ES6 Allows
+  [weekdays[3]]: {
     open: 12,
         close: 22,
   },
@@ -29,9 +30,11 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  // Allowed by ES6
   openingHours,
 
-  order: function(starterIndex, mainIndex) {
+  // ES6 allows this function
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
