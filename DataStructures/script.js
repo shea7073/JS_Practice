0cc35f9f6f71d9933b489ec9a2a4cdf019d1e546
shea7073 <shea7073@gmail.com>
2022-04-22 11:05:55 -0400

    mac

--- DataStructures/script.js
+++ DataStructures/script.js
@@ -477,63 +477,237 @@ GOOD LUCK 😀
 
 // MAPS
 
-const rest = new Map();
-rest.set('name', 'classico italiano');
-rest.set(1, 'Firenze');
-// set actually also returns the map
-console.log(rest.set(2, 'lisbon'));
+// const rest = new Map();
+// rest.set('name', 'classico italiano');
+// rest.set(1, 'Firenze');
+// // set actually also returns the map
+// console.log(rest.set(2, 'lisbon'));
+//
+// rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']).set('open', 11).set('close', 23);
+// rest.set(true, 'we are open').set(false, 'we are closed');
+// // reading from maps
+// console.log(rest.get('name'));
+//
+// const time = 21;
+//
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+//
+// // check if map has element
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// //rest.size;
+// //rest.clear();
+//
+// // can use array as key but it has to be the same array in memory (so save the array in a variable first)
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// console.log(rest.get(arr));
+// // objects too!
+// console.log(document.querySelector('h1'), 'Heading');
+//
+// // map iteration
+// // looks a lot like Object.entries
+// const question = new Map([
+//     ['question', 'what is the best programming language?'],
+//     [1, 'C'],
+//     [2, 'Java'],
+//     [3, 'Javascript'],
+//     ['correct', 3],
+//     [true, 'correct'],
+//     [false, 'try again']
+//     ]);
+//
+// // convert object to map
+// const hoursMap = new Map(Object.entries(openingHours));
+//
+// // quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') {
+//     console.log(`answer ${key}: ${value}`);
+//   }
+// }
+// // const answer = Number(prompt('Your answer'));
+// const answer = 3;
+// console.log(answer);
+//
+// console.log(question.get(answer === question.get('correct')));
+//
+// // convert map to array
+// console.log([...question]);
+// console.log([...question.keys()]);
+// console.log([...question.values()]);
 
-rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']).set('open', 11).set('close', 23);
-rest.set(true, 'we are open').set(false, 'we are closed');
-// reading from maps
-console.log(rest.get('name'));
+// Coding Challenge #3
 
-const time = 21;
+/*
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
 
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
 
-// check if map has element
-console.log(rest.has('categories'));
-rest.delete(2);
-//rest.size;
-//rest.clear();
+GOOD LUCK 😀
+*/
 
-// can use array as key but it has to be the same array in memory (so save the array in a variable first)
-const arr = [1, 2];
-rest.set(arr, 'Test');
-console.log(rest.get(arr));
-// objects too!
-console.log(document.querySelector('h1'), 'Heading');
+// const gameEvents = new Map([
+//     [17, '⚽️ GOAL'],
+//     [36, '🔁 Substitution'],
+//     [47, '⚽️ GOAL'],
+//     [61, '🔁 Substitution'],
+//     [64, '🔶 Yellow card'],
+//     [69, '🔴 Red card'],
+//     [70, '🔁 Substitution'],
+//     [72, '🔁 Substitution'],
+//     [76, '⚽️ GOAL'],
+//     [80, '⚽️ GOAL'],
+//     [92, '🔶 Yellow card'],
+// ]);
+//
+// // 1.
+//
+// const events = [...new Set(gameEvents.values())];
+// console.log(gameEvents.values());
+// console.log(events);
+//
+// // 2.
+//
+// gameEvents.delete(64);
+// console.log(gameEvents);
+//
+// // 3.
+//
+// console.log(`An event happened, on average, every ${90 / gameEvents.size} minutes`);
+//
+// // 4.
+//
+// for (const [key, value] of gameEvents) {
+//     const half = (key > 45) ? "SECOND HALF" : "FIRST HALF";
+//     console.log(`[${half}] ${key}: ${value}`);
+// }
 
-// map iteration
-// looks a lot like Object.entries
-const question = new Map([
-    ['question', 'what is the best programming language?'],
-    [1, 'C'],
-    [2, 'Java'],
-    [3, 'Javascript'],
-    ['correct', 3],
-    [true, 'correct'],
-    [false, 'try again']
-    ]);
+// WORKING WITH STRINGS
 
-// convert object to map
-const hoursMap = new Map(Object.entries(openingHours));
+// methods (indexOf, lastIndexOf) can use for characters or whole words
+// properties (length)
 
-// quiz app
-console.log(question.get('question'));
-for (const [key, value] of question) {
-  if (typeof key === 'number') {
-    console.log(`answer ${key}: ${value}`);
-  }
-}
-// const answer = Number(prompt('Your answer'));
-const answer = 3;
-console.log(answer);
+// const airline = 'TAP Air Portugal';
+// // const plane = 'A320';
+//
+// console.log(airline.slice(4)); // does not alter the string since they are immutable
+//
+// console.log(airline.slice(4, 7)); // start at 4 and end at 7 non inclusive
+//
+// console.log(airline.slice(0, airline.indexOf(' ')));
+//
+// console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+//
+// console.log(airline.slice(-2));
+//
+// console.log(airline.slice(1, -1));
+//
+// const checkMiddleSeat = function(seat) {
+//     // B and E are middle seats
+//     const s = seat.slice(-1);
+//     return s === 'B' || s === 'E';
+// }
+//
+// console.log(checkMiddleSeat('11B'));
+// console.log(checkMiddleSeat('23C'));
+//
+// console.log(airline.toUpperCase());
+// console.log(airline.toLowerCase());
+//
+// const passenger = 'jOnAs';
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+//
+// // comparing emails
+//
+// const email = 'hello@jonas.io';
+// const loginEmail = ' Hello@Jonas.Io \n';
+//
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+//
+// const priceGB = '288,97£'
+// const priceUS = priceGB.replace('£', '$').replace(',', '.');
+//
+// console.log(priceUS);
+//
+// const announcement = `All passengers come to boarding door 23, thats boarding door 23!`;
+// console.log(announcement.replaceAll('door', 'gate'));
+// // could also use regex
+//
+// const plane = 'Airbus A320neo'
+// console.log(plane.includes('A320'));
+// console.log(plane.startsWith('A'));
+//
+// if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+//     console.log('Part of new airbus family');
+// }
+//
+// // practice
+//
+// const checkBaggage = function(items) {
+//     const baggage = items.toLowerCase();
+//     if (baggage.includes('knife') || baggage.includes('gun') {
+//         console.log('you are not allowed onboard!')
+//     }
+//     else {
+//         console.log('Welcome aboard!');
+//     }
+// }
 
-console.log(question.get(answer === question.get('correct')));
+// PART 3 WORKING WITH STRINGS
 
-// convert map to array
-console.log([...question]);
-console.log([...question.keys()]);
-console.log([...question.values()])
+// console.log('a+very+nice+string'.split('+'));
+//
+// const [firstName, lastName] = 'Sean Shea'.split(' ');
+// console.log(firstName, lastName);
+//
+// const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+// console.log(newName);
+//
+// const capitalizeName = function(name) {
+//     const names = name.split(' ');
+//
+//     const namesUpper = [];
+//
+//     for (const n of names) {
+//         // one way >> namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//
+//         namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//     }
+//     console.log(namesUpper.join(' '));
+// }
+//
+// capitalizeName('jessica ann smith davis');
+//
+// // padding
+//
+// const message = 'go to gate 23!'
+// console.log(message.padStart(25, '+').padEnd(35, '+'));
+//
+// const maskCreditCard = function(number) {
+//     const str = number + '';
+//     const last = str.slice(-4);
+//     return last.padStart(str.length, '*')
+// }
+//
+// console.log(maskCreditCard(343434565654));
+//
+// // repeat
+//
+// const message2 = 'Bad weather... All departures delayed... ';
+// console.log(message2.repeat(5));
+//
+// const planesInLine = function (n) {
+//     console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+// }
+//
+// planesInLine(10);
+
